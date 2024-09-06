76cb66505c2672d6e6ee07e6be97ff0d133fa079
MolnKriszt <krisz200601@gmail.com>
2024-09-06 12:23:33 +0200

    asd

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
-import BemutatkozoKartya from './components/BemutatkozoKartya.vue';
-
+import BemutatkozoKartya from './components/BemutatkozoKartya.vue'
 </script>
-
+ 
 <template>
-<div class="container">
   <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+ 
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      
+      
 
       <nav>
         <RouterLink to="/">Home </RouterLink>
         <RouterLink to="/dorian">Dorián </RouterLink>
         <RouterLink to="/kiki">Kiki </RouterLink>
         <RouterLink to="/janos">János </RouterLink>
+ 
       </nav>
-
-    </header>
-  </div>
-
-
+    </div>
+  </header>
+ 
+  <RouterView />
 </template>
