2e29473c8762ee9dac956634a00a88d7c34444a8
feherdorian <dorianfeherzsolt@gmail.com>
2024-09-06 10:04:45 +0200

    helo

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+//icon
+import "bootstrap-icons/font/bootstrap-icons.min.css"
+
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
