df4a6f32ad9b32112a75cd6a7602e3ab9dfba962
MolnKriszt <krisz200601@gmail.com>
2024-09-07 22:53:43 +0200

    ggwp

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,46 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import BemutatkozoKartya from './components/BemutatkozoKartya.vue'
-
-// a másik megoldás
 import { useRouter } from 'vue-router';
+import { ref } from 'vue';
 
+// Router használata
 const router = useRouter();
 
-const navigateHome = () => {
-  router.push('/');
-};
-const navigateKrisztian = () => {
-  router.push('/kiki');
-};
-const navigateJanos = () => {
-  router.push('/janos');
-};
-const navigateDorian = () => {
-  router.push('/dorian');
-};
+// Állapot létrehozása a kiválasztott elemhez
+const selectedElement = ref('home');
 
+// Navigációs és színkezelő függvények
+const handleClick = (element) => {
+  selectedElement.value = element; // Beállítjuk a kiválasztott elemet
+
+  // Itt az egyes elemekhez külön navigációt adunk
+  switch (element) {
+    case 'home':
+      router.push('/');
+      break;
+    case 'dorian':
+      router.push('/dorian');
+      break;
+    case 'janos':
+      router.push('/janos');
+      break;
+    case 'krisztian':
+      router.push('/kiki');
+      break;
+  }
+};
 </script>
+
 
 <template>
   <header>
-    <!-- NAVBAR -->
     <nav class="navbar bg-body-tertiary my-navbar">
       <div class="container-fluid p-0">
-        <a class="navbar-brand ps-3" href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+        <a class="navbar-brand ps-3" href="#" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample"
+          aria-controls="offcanvasExample">
           <i class="bi bi-list-nested"></i>
         </a>
       </div>
     </nav>
-
 
     <div class="container-fluid p-0">
       <RouterView />
@@ -40,75 +48,38 @@ const navigateDorian = () => {
       <div class="offcanvas offcanvas-start container-fluid p-0 list-element" tabindex="-1" id="offcanvasExample"
         aria-labelledby="offcanvasExampleLabel">
 
-        <div class="offcanvas-header">
+        <div class="offcanvas-header my-offcanvas-header">
           <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-
-
         </div>
-
         <div class="offcanvas-body p-0">
-          <!-- a másik megoldás -->
-          <div class="container-fliud d-flex justify-content-center p-5 my-element" @click="navigateHome"
-            style="min-width:100%; min-height: 100px; cursor: pointer;">
-            <p class="my-font-style home-button">
-              HOME
-            </p>
+          <div :class="{
+            'container-fluid d-flex justify-content-center align-items-center p-5 my-element': true,
+            'clicked': selectedElement === 'home'
+          }" style="min-width:100%; min-height: 25%; cursor: pointer;" @click="handleClick('home')">
+            <p class="my-font-style home-button">HOME</p>
+          </div>
+          <div :class="{
+            'container-fluid d-flex justify-content-center align-items-center p-5 my-element': true,
+            'clicked': selectedElement === 'dorian'
+          }" style="min-width:100%; min-height: 25%; cursor: pointer;" @click="handleClick('dorian')">
+            <p class="my-font-style">Dorián</p>
           </div>
 
-          <!-- a régi megoldás -->
-          <!-- <div class="container-fliud d-flex justify-content-center pt-5" onclick=""
-          style=" min-width:100%; min-height: 100px;">
-          <RouterLink to="/" class="text-reset text-decoration-none">Home</RouterLink>
-        </div> -->
+          <div :class="{
+            'container-fluid d-flex justify-content-center align-items-center p-5 my-element': true,
+            'clicked': selectedElement === 'janos'
+          }" style="min-width:100%; min-height: 25%; cursor: pointer;" @click="handleClick('janos')">
+            <p class="my-font-style">János</p>
+          </div>
 
-
-          <div class="container overflow-hidden text-center">
-            <div class="row">
-              <div class="gy-1">
-                <div class="col-12">
-                  <div class="p-3">
-                    <div class="emberek-button justify-content-center p-2 my-element" 
-                      @click="navigateDorian">
-                      Dorián
-                      <!-- regi -->
-                      <!-- <RouterLink to="/dorian" class="text-reset text-decoration-none">Dorián</RouterLink> -->
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="gy-4">
-                <div class="col-12">
-                  <div class="p-3">
-                    <div class="emberek-button container-fliud d-flex justify-content-center p-2 my-element"
-                      @click="navigateKrisztian">
-                      Krisztián
-                      <!-- regi -->
-                      <!-- <RouterLink to="/kiki" class="text-reset text-decoration-none">Kiki</RouterLink> -->
-                    </div>
-
-                  </div>
-                </div>
-              </div>
-              <div class="gy-4">
-                <div class="col-12">
-                  <div class="p-3">
-                    <div class="emberek-button container-fliud d-flex justify-content-center p-2 my-element"
-                      @click="navigateJanos">
-                      János
-                      <!-- regi -->
-                      <!-- <RouterLink to="/janos" class="text-reset text-decoration-none">János</RouterLink> -->
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-
-            </div>
+          <div :class="{
+            'container-fluid d-flex justify-content-center align-items-center p-5 my-element': true,
+            'clicked': selectedElement === 'krisztian'
+          }" style="min-width:100%; min-height: 25%; cursor: pointer;" @click="handleClick('krisztian')">
+            <p class="my-font-style">Krisztián</p>
           </div>
         </div>
-
       </div>
-
     </div>
   </header>
 </template>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,13 @@
 <template>
     <h1>HOME</h1>
+    <!-- regi -->
+                      <!-- <RouterLink to="/dorian" class="text-reset text-decoration-none">Dorián</RouterLink> -->
+                       
+
+    <!-- regi -->
+                      <!-- <RouterLink to="/kiki" class="text-reset text-decoration-none">Kiki</RouterLink> -->
+    <!-- regi -->
+                      <!-- <RouterLink to="/janos" class="text-reset text-decoration-none">János</RouterLink> -->
   </template>
   
   <script>
