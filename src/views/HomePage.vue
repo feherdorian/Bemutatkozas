769568b58ca557ceae54c772e3bf85937cf2fc33
MolnKriszt <krisz200601@gmail.com>
2024-09-08 21:01:02 +0200

    halo

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="container-fluid p-0">
-    <div class="d-flex justify-content-center">
-      <h1 class="home-felirat">
-        Kovács János & Fehér Zsolt Dorián & Molnár Krisztián
+    <div class="d-flex justify-content-start home-felirat-div">
+      <h1>
+        <a href="https://github.com/janesz315" class="my-name" target="_blank">Kovács János</a><span class="and"> &</span><br>
+        <a href="https://github.com/feherdorian" class="my-name" target="_blank">Fehér Zsolt Dorián</a><span class="and">
+          &</span><br>
+        <a href="https://github.com/MolnKriszt" class="my-name" target="_blank">Molnár Krisztián</a>
       </h1>
+
     </div>
 
     <!-- <h1>HOME</h1> -->
-    <!-- <img src="/public/csoportkep.png" class="img-fluid" alt="csoportkep.png"> -->
+    <img src="/public/csoportkep.png" class="img-fluid my-image" alt="csoportkep.png">
     <!-- <p class="my-clock">{{ currentTime }}</p> -->
   </div>
 </template>
