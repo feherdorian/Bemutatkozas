20a7dcf9df566758ed70d6b7c1890282e88f50ee
MolnKriszt <krisz200601@gmail.com>
2024-09-08 21:03:05 +0200

    facebook

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,10 +2,10 @@
   <div class="container-fluid p-0">
     <div class="d-flex justify-content-start home-felirat-div">
       <h1>
-        <a href="https://github.com/janesz315" class="my-name" target="_blank">Kovács János</a><span class="and"> &</span><br>
-        <a href="https://github.com/feherdorian" class="my-name" target="_blank">Fehér Zsolt Dorián</a><span class="and">
+        <a href="https://www.facebook.com/profile.php?id=100017469883086" class="my-name" target="_blank">Kovács János</a><span class="and"> &</span><br>
+        <a href="https://www.facebook.com/profile.php?id=100010022708789" class="my-name" target="_blank">Fehér Zsolt Dorián</a><span class="and">
           &</span><br>
-        <a href="https://github.com/MolnKriszt" class="my-name" target="_blank">Molnár Krisztián</a>
+        <a href="https://www.facebook.com/krisztian.molnar.5437/" class="my-name" target="_blank">Molnár Krisztián</a>
       </h1>
 
     </div>
